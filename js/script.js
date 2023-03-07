a0567a18edce950dfe51720fc6c2c6357bf08987
Rodrigooou <rm550816@fiap.com.br>
2023-03-07 11:49:07 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Este é um comentario de linha
+
+/** 
+ * É um comentario 
+ * de
+ * Bloco
+*/
+
+console.log("Se presta atenção nessa bagaça")
+console.error("Ta viajando que eu sei")
+console.warn("Vc foi avisado")
+console.info("VISH PARÇA")
